d85736d96ce810e7f8f3c4b117d8fd8ba099f5ed
hennachen <henna.chen22@gmail.com>
2018-05-21 09:22:57 +1000

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,21 @@
+
+package tictactoe;
+
+
+public class Player {
+    
+    private final int PLAYER1 = 1; 
+    private final int PLAYER2 = 2; 
+    
+    private final String name;
+    private int turnNum; 
+    private Player nextPlayer;
+    
+    public Player(String name){
+        this.name = name; 
+    }
+    
+    //Determine which player 
+    
+    
+}
---
+++ TicTacToe.java
@@ -0,0 +1,12 @@
+
+package tictactoe;
+
+
+public class TicTacToe {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
