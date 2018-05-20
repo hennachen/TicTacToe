e00a594520a91fd7dee39965e3b81a691b9dfd33
hennachen <henna.chen22@gmail.com>
2018-05-21 09:32:12 +1000

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,11 +2,44 @@
 package tictactoe;
 
 
-public class TicTacToe {
+public class TicTacToe implements GameInterface {
 
     
     public static void main(String[] args) {
         
+    }
+
+    @Override
+    public void beginGame() {
+    }
+
+    @Override
+    public void playerMove() {
+    }
+
+    @Override
+    public void playerMoveResult() {
+    }
+
+    @Override
+    public boolean isEmptyCells() {
+        return false;
+    }
+
+    @Override
+    public void currentState() {
+    }
+
+    @Override
+    public void showGame() {
+    }
+
+    @Override
+    public void showCell(int move) {
+    }
+
+    @Override
+    public void winner() {
     }
     
 }
